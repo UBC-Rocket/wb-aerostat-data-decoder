cc4d3750f0fdbb019653b69a40275134bf9b32ed
stonelips <stonelips@users.noreply.github.com>
2021-11-20 10:06:11 -0800

    Fixed glitches on two transmissions in data (by doing some manual format editing + using unicode escape decoding)

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 Data decoder for WB Aerostat subteam. Converts raw APRS packets into a form usable by the wb_wind_analysis repo.
 
 """
-import analyzer_sd, analyzer_direwolf
+import analyzer_direwolf
 
 # Make sure to set TIME_STEP in config.
-t = analyzer_direwolf.AprsFiAnalyzer("resources/launchData/launch_1_aprsFi_fixed.csv", 15)
-t.output_vectors("l_1_Out_decoded.csv")
-#t.save_datapoints("l_1_Out_datapoints_decoded.csv")
+t = analyzer_direwolf.AprsFiAnalyzer("resources/launchData/launch_1_aprsFi_noQuotes.csv", 15)
+t.output_vectors("outputs/L1_vec_noQ.csv")
+t.save_datapoints("outputs/L1_dp_noQ.csv")
 
-"""v = analyzer_direwolf.DirewolfAnalyzer("direwolfTestFile1July16.csv")
+"""v = analyzer_direwolf.DirewolfAnalyzer("outputs/direwolfTestFile1July16.csv")
 v.output_vectors()"""
